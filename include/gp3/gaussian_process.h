ce3f56236c87f2dd3a5e9452aec6c40dd82cdc70
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2017-05-01 02:40:14 -0700

    got most of the backend together

--- include/gp3/gaussian_process.h
+++ include/gp3/gaussian_process.h
@@ -11,133 +11,49 @@
 typedef std::pair<ros::Time, double> Data;
 
 class Kernel {
-  Kernel(const double& lengthscale_sd, const double& signal_sd)
-      : lengthscale_var_(lengthscale_sd * lengthscale_sd),
-        signal_var_(signal_sd * signal_sd){};
+ public:
+  Kernel(const double& lengthscale_sd, const double& signal_sd);
+
+  virtual double operator()(const double& a, const double& b) const;
 
  protected:
   double lengthscale_var_;
   double signal_var_;
-
- public:
-  virtual double operator()(const double& a, const double& b) const;
 };
 
 class SqExpKernel : public Kernel {
  public:
-  double operator()(const double& a, const double& b) const {
-    double sq_dist = (a - b) * (a - b);
-    return signal_var_ * std::exp(-sq_dist / (2 * lengthscale_var_));
-  }
+  SqExpKernel(const double& lengthscale_sd, const double& signal_sd);
+  double operator()(const double& a, const double& b) const;
 };
 
-// solves wrap around issue for angles
 class SqExpAngleWrapKernel : public Kernel {
  public:
-  double operator()(const double& a, const double& b) const {
-    double abs_dist = std::abs(a - b);
-    abs_dist = std::min(abs_dist, std::abs(abs_dist - 2 * M_PI));
-    double sq_dist = abs_dist * abs_dist;
-    return signal_var_ * std::exp(-sq_dist / (2 * lengthscale_var_));
-  }
+  SqExpAngleWrapKernel(const double& lengthscale_sd, const double& signal_sd);
+  double operator()(const double& a, const double& b) const;
 };
 
 class GaussianProcess {
  public:
-  GaussianProcess(const Kernel& kernel, const size_t& max_data_points)
-      : kernel_(kernel),
-        max_data_points_(max_data_points),
-        ready_to_predict_(false){};
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  GaussianProcess(
+      const Kernel& kernel, const size_t& max_data_points,
+      const double& time_diff_warning = std::numeric_limits<double>::max());
 
   void addDataPoint(const ros::Time& time, const double& data_point,
-                    const bool recompute_cov = true) {
-    data_list_.emplace_back(time, data_point);
+                    const bool recompute_cov = true);
 
-    if (data_list_.size() > max_data_points_) {
-      data_list_.pop_front();
-    }
-    ready_to_predict_ = false;
-
-    if (recompute_cov) {
-      computeCovariance();
-    }
-  }
-
-  void computeCovariance() {
-    if (data_list_.size() == 0) {
-      throw std::runtime_error(
-          "Covariance requested before system had any data");
-    }
-
-    Eigen::MatrixXd K;
-    K.resize(data_list_.size(), data_list_.size());
-
-    // convert times into times since first reading in list
-    time_diffs_.resize(data_list_.size());
-    std::vector<double>::iterator time_it = time_diffs_.begin();
-    for (const Data& data : data_list_) {
-      *time_it = (data.first - data_list_.front().first).toSec();
-      ++time_it;
-    }
-
-    // loop over computing each element. Only lower triangular elements are
-    // computed as the others won't be used.
-    for (size_t i = 0; i < data_list_.size(); ++i) {
-      for (size_t j = 0; j <= i; ++j) {
-        K(i, j) = kernel_(time_diffs_[i], time_diffs_[j]);
-      }
-    }
-
-    // use cholesky decomposition and get llt of lower triangular matrix
-    L_llt_ = Eigen::MatrixXd(K.llt().matrixL()).llt();
-
-    // calculate mu
-    Eigen::MatrixXd data_values;
-    data_values.resize(data_list_.size(), 1);
-
-    std::list<Data>::iterator data_it = data_list_.begin();
-    for (size_t i = 0; i < data_list_.size(); ++i) {
-      data_values(i, 1) = data_it->second;
-      ++data_it;
-    }
-    mu_ = L_llt_.solve(data_values);
-
-    ready_to_predict_ = true;
-  }
+  void computeCovariance();
 
   // get the prediction at the specified time
   void predict(const ros::Time& prediction_time, double* predicted_value,
-               double* prediction_sd) {
-    if (!ready_to_predict_) {
-      throw std::runtime_error("Prediction requested before system was ready");
-    }
-
-    double prediction_time_diff =
-        (prediction_time - data_list_.front().first).toSec();
-
-    Eigen::MatrixXd K_s;
-    K_s.resize(1, data_list_.size());
-    for (size_t i = 0; i < data_list_.size(); ++i) {
-      K_s(1, i) = kernel_(time_diffs_[i], prediction_time_diff);
-    }
-
-    Eigen::MatrixXd L_k = L_llt_.solve(K_s);
-
-    *predicted_value = 0;
-    for (size_t i = 0; i < data_list_.size(); ++i) {
-      *predicted_value += L_k(i, 1) + mu_(i, 1);
-    }
-
-    *prediction_sd = kernel_(prediction_time_diff, prediction_time_diff);
-    for (size_t i = 0; i < data_list_.size(); ++i) {
-      *prediction_sd -= L_k(i, 1) * L_k(i, 1);
-    }
-    *prediction_sd = std::sqrt(*prediction_sd);
-  }
+               double* prediction_sd = nullptr);
 
  private:
   const Kernel kernel_;
   const size_t max_data_points_;
+  const double time_diff_warning_;
 
   std::list<Data> data_list_;
   std::vector<double> time_diffs_;
@@ -147,7 +63,7 @@ class GaussianProcess {
   bool ready_to_predict_;
 
   // needed for predicting values
-  Eigen::MatrixXd mu_;
+  Eigen::VectorXd mu_;
 };
 
 #endif  // GP3_GAUSSIAN_PROCESS_H
--- include/gp3/gp3.h
+++ include/gp3/gp3.h
@@ -1,2 +1,172 @@
+#include <ros/ros.h>
 
-#include <Eigen/Eigen>
+#include "geometry_msgs/TransformStamped.h"
+#include "gp3/gaussian_process.h"
+#include "kindr/minimal/quat-transformation.h"
+#include "minkindr_conversions/kindr_msg.h"
+
+constexpr int kDefaultMaxDataPoints = 100;
+constexpr double kDefaultTimeDiffWarning = 0.25;
+
+class GP3 {
+ public:
+  struct Config {
+    double x_lengthscale_;
+    double y_lengthscale_;
+    double z_lengthscale_;
+
+    double rx_lengthscale_;
+    double ry_lengthscale_;
+    double rz_lengthscale_;
+
+    double x_signal_noise_sd_;
+    double y_signal_noise_sd_;
+    double z_signal_noise_sd_;
+
+    double rx_signal_noise_sd_;
+    double ry_signal_noise_sd_;
+    double rz_signal_noise_sd_;
+
+    int max_data_points_;
+    double time_diff_warning_;
+  };
+
+  static Config readConfig(ros::NodeHandle nh, bool group_lengthscale = true,
+                           bool group_signal_noise_sd = true) {
+    Config config;
+
+    if (group_lengthscale) {
+      double position_lengthscale = readValue(nh, "position_lengthscale");
+      double orientation_lengthscale = readValue(nh, "orientation_lengthscale");
+
+      config.x_lengthscale_ = position_lengthscale;
+      config.y_lengthscale_ = position_lengthscale;
+      config.z_lengthscale_ = position_lengthscale;
+
+      config.rx_lengthscale_ = orientation_lengthscale;
+      config.ry_lengthscale_ = orientation_lengthscale;
+      config.rz_lengthscale_ = orientation_lengthscale;
+    } else {
+      config.x_lengthscale_ = readValue(nh, "x_lengthscale");
+      config.y_lengthscale_ = readValue(nh, "y_lengthscale");
+      config.z_lengthscale_ = readValue(nh, "z_lengthscale");
+
+      config.rx_lengthscale_ = readValue(nh, "rx_lengthscale");
+      config.ry_lengthscale_ = readValue(nh, "ry_lengthscale");
+      config.rz_lengthscale_ = readValue(nh, "rz_lengthscale");
+    }
+
+    if (group_signal_noise_sd) {
+      double position_signal_noise_sd =
+          readValue(nh, "position_signal_noise_sd");
+      double orientation_signal_noise_sd =
+          readValue(nh, "orientation_signal_noise_sd");
+
+      config.x_signal_noise_sd_ = position_signal_noise_sd;
+      config.y_signal_noise_sd_ = position_signal_noise_sd;
+      config.z_signal_noise_sd_ = position_signal_noise_sd;
+
+      config.rx_signal_noise_sd_ = orientation_signal_noise_sd;
+      config.ry_signal_noise_sd_ = orientation_signal_noise_sd;
+      config.rz_signal_noise_sd_ = orientation_signal_noise_sd;
+    } else {
+      config.x_signal_noise_sd_ = readValue(nh, "x_signal_noise_sd");
+      config.y_signal_noise_sd_ = readValue(nh, "y_signal_noise_sd");
+      config.z_signal_noise_sd_ = readValue(nh, "z_signal_noise_sd");
+
+      config.rx_signal_noise_sd_ = readValue(nh, "rx_signal_noise_sd");
+      config.ry_signal_noise_sd_ = readValue(nh, "ry_signal_noise_sd");
+      config.rz_signal_noise_sd_ = readValue(nh, "rz_signal_noise_sd");
+    }
+
+    nh.param("max_data_points", config.max_data_points_, kDefaultMaxDataPoints);
+    if (config.max_data_points_ <= 0) {
+      throw std::runtime_error("Max data points value must be greater than 0");
+    }
+    nh.param("time_diff_warning", config.time_diff_warning_,
+             kDefaultTimeDiffWarning);
+
+    return config;
+  }
+
+  static double readValue(ros::NodeHandle nh, std::string value_name) {
+    double value;
+    if (nh.getParam(value_name, value)) {
+      throw std::runtime_error(std::string("Could not find ") + value_name +
+                               " in parameter server");
+    }
+    if (value <= 0) {
+      throw std::runtime_error(std::string("Value for ") + value_name +
+                               " must be greater than 0");
+    }
+    return value;
+  }
+
+  GP3(const Config& config)
+      : x_gp_(SqExpKernel(config.x_lengthscale_, config.x_signal_noise_sd_),
+              config.max_data_points_, config.time_diff_warning_),
+        y_gp_(SqExpKernel(config.y_lengthscale_, config.y_signal_noise_sd_),
+              config.max_data_points_),
+        z_gp_(SqExpKernel(config.z_lengthscale_, config.z_signal_noise_sd_),
+              config.max_data_points_),
+        rx_gp_(SqExpAngleWrapKernel(config.rx_lengthscale_,
+                                    config.rx_signal_noise_sd_),
+               config.max_data_points_),
+        ry_gp_(SqExpAngleWrapKernel(config.ry_lengthscale_,
+                                    config.ry_signal_noise_sd_),
+               config.max_data_points_),
+        rz_gp_(SqExpAngleWrapKernel(config.rz_lengthscale_,
+                                    config.rz_signal_noise_sd_),
+               config.max_data_points_) {}
+
+  void addTransformMeasurement(
+      const geometry_msgs::TransformStamped& tform_msg) {
+    kindr::minimal::QuatTransformation tform;
+    tf::transformMsgToKindr(tform_msg.Transform, &tform);
+
+    Eigen::Matrix<double, 6, 1> tform_log = tform.log();
+    x_gp_.addDataPoint(tform.header.stamp, tform_log[0]);
+    y_gp_.addDataPoint(tform.header.stamp, tform_log[1]);
+    z_gp_.addDataPoint(tform.header.stamp, tform_log[2]);
+
+    rx_gp_.addDataPoint(tform.header.stamp, tform_log[3]);
+    ry_gp_.addDataPoint(tform.header.stamp, tform_log[4]);
+    rz_gp_.addDataPoint(tform.header.stamp, tform_log[5]);
+  }
+
+  void predictTransform(const ros::Time& prediction_time,
+                        Eigen::Matrix<double, 6, 1>* tform_log) {
+    x_gp_.predict(prediction_time, &(tform_log->coeffRef(0)));
+    y_gp_.predict(prediction_time, &(tform_log->coeffRef(1)));
+    z_gp_.predict(prediction_time, &(tform_log->coeffRef(2)));
+
+    rx_gp_.predict(prediction_time, &(tform_log->coeffRef(3)));
+    ry_gp_.predict(prediction_time, &(tform_log->coeffRef(4)));
+    rz_gp_.predict(prediction_time, &(tform_log->coeffRef(5)));
+  }
+
+  void predictTransform(const ros::Time& prediction_time,
+                        kindr::minimal::QuatTransformation* tform) {
+    Eigen::Matrix<double, 6, 1> tform_log;
+    predictTransform(prediction_time, &tform_log);
+    *tform = kindr::minimal::QuatTransformation::exp(tform_log);
+  }
+
+  void predictTransform(const ros::Time& prediction_time,
+                        geometry_msgs::TransformStamped* tform_msg) {
+    kindr::minimal::QuatTransformation tform;
+    predictTransform(prediction_time, &tform);
+    
+    tf::transformKindrToMsg(tform, &(tform_msg->transform));
+    tform_msg->header.stamp = prediction_time;
+  }
+
+ private:
+  GaussianProcess x_gp_;
+  GaussianProcess y_gp_;
+  GaussianProcess z_gp_;
+
+  GaussianProcess rx_gp_;
+  GaussianProcess ry_gp_;
+  GaussianProcess rz_gp_;
+};
